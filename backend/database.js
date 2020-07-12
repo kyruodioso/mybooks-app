6aeed7a9d0654d18ab8d8558c598422f5b3d11b7
Cristian Bejarano <kanu_nirvana@hotmail.com>
2020-07-12 09:46:40 -0300

    new commit

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,10 @@
 const mongoose= require('mongoose');
 
-//url database
-const uri= 'mongodb://localhost/api-libros:2707';
-const config= {useNewUrlParser:true,useUnifiedTopology:true};
 
 
-mongoose.connect(process.env.MONGODB_URI
-    , config)
+
+mongoose.connect(process.env.MONGODB_URI, config)
 .then(db=> console.log('database is run'))
 .catch(err=>console.log('error in database'))
 
-module.export = mongoose;
+//module.export = mongoose;

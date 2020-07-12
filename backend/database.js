6e694f9cde7aba884669c6c5cc8e5b41c59cbfa5
Cristian Bejarano <kanu_nirvana@hotmail.com>
2020-07-12 10:03:12 -0300

    new commit

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,10 @@
 const mongoose= require('mongoose');
 
-
+//url database
+const config= {useNewUrlParser:true,useUnifiedTopology:true};
 
 
 mongoose.connect(process.env.MONGODB_URI, config)
 .then(db=> console.log('database is run'))
 .catch(err=>console.log('error in database'))
 
-//module.export = mongoose;
